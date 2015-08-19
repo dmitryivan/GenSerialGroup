29800acd5f7087ad0056eff34e4aaaf103cef39a
dmitryivan <onemore43@gmail.com>
2015-08-19 15:06:40 +0300

    Create GroupInterface.java

---
+++ GroupInterface.java
@@ -0,0 +1,8 @@
+public interface GroupInterface {
+void addStudent(StudentInterface s);
+String getGroupName();
+int getGroupSize();
+StudentInterface searchStudent(String name);
+void sortStudents(String criteria);
+void listGroup();
+}
